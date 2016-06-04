d9b03e89748a3fb48788fe71d0528b3d5a4a73c9
Salar Latifi <salar@umich.edu>
2016-06-04 16:16:26 +0430

    Updated acceleration algorithm of LRN layer & added auto-tuning option

--- CNNdroid Source Package/java/layers/LocalResponseNormalization.java
+++ CNNdroid Source Package/java/layers/LocalResponseNormalization.java
@@ -2,6 +2,11 @@ package layers;
 
 import android.util.Log;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Scanner;
+
 import numdroid.*;
 
 public class LocalResponseNormalization implements LayerInterface {
@@ -13,9 +18,13 @@ public class LocalResponseNormalization implements LayerInterface {
     private double beta;                        // beta
     private String normRegion;                  // norm region: "across_channels"
     private boolean parallel;                   // implementation method (parallel or sequential)
+    private String tuningFolder;                // location to store online tuning results
+    private boolean tune;                       // flag to weather execute tuning ro not
+    private int threadCount;                    // thread count for acceleration
+    private int[] threadCounts = {4, 6, 8};
 
     public LocalResponseNormalization(int localSize, double alpha, double beta, String normRegion,
-                                      boolean parallel, String name) {
+                                      boolean parallel, String name, String tuningFolder) {
         this.localSize = localSize;
         this.alpha = alpha;
         this.beta = beta;
@@ -23,6 +32,18 @@ public class LocalResponseNormalization implements LayerInterface {
         this.parallel = parallel;
         this.name = name;
         myNum = new MyNum();
+        this.tuningFolder = tuningFolder;
+
+        tune = false;
+        File f = new File(tuningFolder + "/" + name + ".txt");
+        try {
+            Scanner s = new Scanner(f);
+            threadCount = Integer.valueOf(s.nextLine());
+            if (corrupted(threadCount))
+                tune = true;
+        } catch (FileNotFoundException e) {
+            tune = true;
+        }
     }
 
     @Override
@@ -34,8 +55,10 @@ public class LocalResponseNormalization implements LayerInterface {
 
         if (!parallel)
             output = lrnLayerSeq((float[][][][])input, localSize, alpha, beta, normRegion);
+        else if (tune)
+            output = tuneFunction((float[][][][]) input);
         else
-            output = lrnLayerMultithread((float[][][][])input, localSize, alpha, beta, normRegion);
+            output = lrnLayerMultithread((float[][][][]) input, localSize, alpha, beta, normRegion, threadCount);
 
         runTime = System.currentTimeMillis() - runTime;
         Log.d("CNNdroid", "layers." + name + ": Computation Run Time = " + String.valueOf(runTime));
@@ -77,7 +100,7 @@ public class LocalResponseNormalization implements LayerInterface {
 
     ///////////////////////////////////////Multithread//////////////////////////////////////////////
     public float[][][][] lrnLayerMultithread(float[][][][] inputBlob, int localSize, double alpha,
-                                             double beta, String normRegion) {
+                                             double beta, String normRegion, int threadCount) {
         // Calculate sizes.
         int n_i = inputBlob.length;
         int c_i = inputBlob[0].length;
@@ -90,53 +113,124 @@ public class LocalResponseNormalization implements LayerInterface {
         // Calculate the result.
         if (normRegion.equals("across_channels"))
         {
-            MultiThreadLrn[] threads = new MultiThreadLrn[n_i];
-            for (int n = 0; n < n_i; ++n)
-            {
-                threads[n] = new MultiThreadLrn(inputBlob, n, c_i, h_i, w_i, localSize, alpha, beta, myNum);
-                threads[n].start();
-            }
+            for (int frame = 0 ; frame < n_i ; frame++) {
+                // Calculate the result
+                MultiThreadLrn[] threads = new MultiThreadLrn[threadCount];
+                for (int thread = 0; thread < threadCount ; ++thread) {
+                    threads[thread] = new MultiThreadLrn(inputBlob, frame, c_i, h_i, w_i, localSize, alpha, beta, myNum, thread, threadCount);
+                    threads[thread].start();
+                }
 
-            while (true)
-            {
-                int n;
-                for (n = 0; n < n_i; ++n)
-                    if (!threads[n].done)
+                while (true) {
+                    int thread;
+                    for (thread = 0; thread < threadCount ; thread++)
+                        if (!threads[thread].done)
+                            break;
+                    if (thread == threadCount)
                         break;
-                if (n == n_i)
-                    break;
-                try
-                {
-                    Thread.sleep(10);
+                    try {
+                        Thread.sleep(5);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
-                catch (InterruptedException e)
-                {
-                    e.printStackTrace();
+
+                for (int thread = 0; thread < threadCount ; ++thread) {
+                    int channelCount = c_i / threadCount;
+                    if (c_i % threadCount != 0)
+                        channelCount += 1;
+                    int cStart = thread * channelCount;
+                    int cEnd = cStart + channelCount;
+                    if (cStart > c_i)
+                        cStart = c_i;
+                    if (cEnd > c_i)
+                        cEnd = c_i;
+
+                    for (int c = cStart ; c < cEnd ; ++c)
+                        outputBlob[frame][c]=threads[thread].outputBlob[c - cStart];
                 }
             }
-
-            for (int n = 0; n < n_i; ++n)
-                outputBlob[n] = threads[n].outputBlob;
         }
 
         return outputBlob;
     }
 
+    /////////////////////////////////////////Tuning Function////////////////////////////////////////
+    private Object tuneFunction(float[][][][] input){
+        Log.d("CNNdroid", "layers." + name + ": Tuning process is starting...");
+        long tuneTime = System.currentTimeMillis();
+
+        tune = false;
+        long[] time = new long[threadCounts.length];
+        for (int i = 0 ; i < threadCounts.length ; i++)
+            time[i] = 0;
+        long temp;
+        int c_i = input[0].length;
+        float[][][][] tuneInput = new float[1][c_i][input[0][0].length][input[0][0][0].length];
+        tuneInput[0] = input[0];
+        Object output = null;
+
+        for (int i = 0; i < 4; i++) {
+            for (int thread = 0 ; thread < threadCounts.length ; thread++) {
+                temp = System.currentTimeMillis();
+                output = lrnLayerMultithread(input, localSize, alpha, beta, normRegion, thread);
+                time[thread] += System.currentTimeMillis() - temp;
+            }
+        }
+
+        int min = 0;
+        for (int i = 0; i < threadCounts.length ; i++)
+            if (time[i] <= time[min])
+                min = i;
+
+        threadCount = threadCounts[min];
+
+        writeFile(threadCount);
+        tuneTime = System.currentTimeMillis() - tuneTime;
+        Log.d("CNNdroid", "layers." + name + ": Tuning process finished in " + tuneTime + "ms.");
+        return output;
+    }
+
+    ////////////////////////////////////////Local Functions/////////////////////////////////////////
+    private boolean corrupted(int threadCount)
+    {
+        for (int i = 0 ; i < threadCounts.length ; i++)
+            if (threadCount == threadCounts[i])
+                return false;
+        return true;
+    }
+    private void writeFile(int threadCount)
+    {
+        File f = new File(tuningFolder + "/" + name + ".txt");
+
+        if(f.exists())
+            f.delete();
+        try {
+            f.createNewFile();
+            FileOutputStream fos = new FileOutputStream(f);
+            fos.write(String.valueOf(threadCount).getBytes());
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
 
 class MultiThreadLrn extends Thread {
     private float[][][][] inputBlob;
-    private int n, c_i, h_i, w_i, localSize;
+    private int frame, c_i, h_i, w_i, localSize;
     private double alpha, beta;
     private MyNum myNum;
+    private int cStart;
+    private int cEnd;
 
     public float[][][] outputBlob;
     public boolean done;
 
-    public MultiThreadLrn(float[][][][] inputBlob, int n, int c_i, int h_i, int w_i, int localSize,
-                          double alpha, double beta, MyNum myNum) {
+    public MultiThreadLrn(float[][][][] inputBlob, int frame, int c_i, int h_i, int w_i, int localSize,
+                          double alpha, double beta, MyNum myNum, int threadNum, int threadCount) {
         this.inputBlob = inputBlob;
-        this.n = n;
+        this.frame = frame;
         this.c_i = c_i;
         this.h_i = h_i;
         this.w_i = w_i;
@@ -145,20 +239,33 @@ class MultiThreadLrn extends Thread {
         this.beta = beta;
         this.myNum = myNum;
 
-        outputBlob = new float[c_i][h_i][w_i];
+        int channelCount = c_i / threadCount;
+        if (c_i % threadCount != 0)
+            channelCount += 1;
+        cStart = threadNum * channelCount;
+        cEnd = cStart + channelCount;
+        if (cStart > c_i)
+            cStart = c_i;
+        if (cEnd > c_i)
+            cEnd = c_i;
+
+        channelCount = cEnd - cStart;
+
+
+        outputBlob = new float[channelCount][h_i][w_i];
     }
 
     @Override
     public void run() {
-        for (int c = 0; c < c_i; ++c)
+        for (int c = cStart; c < cEnd ; ++c)
             // For first few channels, do zero padding.
             if (c < (localSize - 1) / 2)
-                outputBlob[c] = myNum.divide(inputBlob[n][c], myNum.power(myNum.sum(myNum.multiply(myNum.sum(myNum.power(inputBlob, n, 0, c + (localSize - 1) / 2 + 1, h_i, w_i, 2)), (float)alpha / localSize), 1), beta));
+                outputBlob[c - cStart] = myNum.divide(inputBlob[frame][c], myNum.power(myNum.sum(myNum.multiply(myNum.sum(myNum.power(inputBlob, frame, 0, c + (localSize - 1) / 2 + 1, h_i, w_i, 2)), (float)alpha / localSize), 1), beta));
                 // For last few channels, do zero padding.
             else if (c > c_i - (localSize - 1) / 2 - 1)
-                outputBlob[c] = myNum.divide(inputBlob[n][c], myNum.power(myNum.sum(myNum.multiply(myNum.sum(myNum.power(inputBlob, n, c - (localSize - 1) / 2, c_i, h_i, w_i, 2)), (float)alpha / localSize), 1), beta));
+                outputBlob[c - cStart] = myNum.divide(inputBlob[frame][c], myNum.power(myNum.sum(myNum.multiply(myNum.sum(myNum.power(inputBlob, frame, c - (localSize - 1) / 2, c_i, h_i, w_i, 2)), (float)alpha / localSize), 1), beta));
             else
-                outputBlob[c] = myNum.divide(inputBlob[n][c], myNum.power(myNum.sum(myNum.multiply(myNum.mean(myNum.power(inputBlob, n, c - (localSize - 1) / 2, c + (localSize - 1) / 2 + 1, h_i, w_i, 2)), (float) alpha), 1), beta));
+                outputBlob[c - cStart] = myNum.divide(inputBlob[frame][c], myNum.power(myNum.sum(myNum.multiply(myNum.mean(myNum.power(inputBlob, frame, c - (localSize - 1) / 2, c + (localSize - 1) / 2 + 1, h_i, w_i, 2)), (float) alpha), 1), beta));
 
         done = true;
     }
